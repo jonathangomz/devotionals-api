d2dca09b72649e6c38c54551c8f79ba40eecadc0
Jonathan <jonicgp97@gmail.com>
2020-09-08 17:04:24 -0500

    Separate app from server

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
-
 app.use(express.json());
 
 app.get("", (req, res) => res.json({
@@ -19,5 +18,4 @@ app.get("/api/v1/", (req, res) => {
 const {path, router} = require("./Endpoints/Books");
 app.use(path, router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
