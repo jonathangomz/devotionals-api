cc99e5ae7fd92a99b9611c00b36f62247abfa908
Jonathan <jonicgp97@gmail.com>
2020-09-09 12:56:43 -0500

    Add log

--- src/Endpoints/Auth.js
+++ src/Endpoints/Auth.js
@@ -16,6 +16,7 @@ router.get('/login', passport.authenticate('auth0', {
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/api/v1/books'
 router.get('/callback', function (req, res, next) {
+  console.log("On Callback on API");
   passport.authenticate('auth0', function (err, user, info) {
     if (err) {
       return next(err);
