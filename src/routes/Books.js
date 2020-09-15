254817d3895cdde671f7d5781855d6147b4bb6ff
Jonathan <jonicgp97@gmail.com>
2020-09-15 12:40:04 -0500

    Add endpoint for single endpoint
    and beautify

--- src/routes/Books.js
+++ src/routes/Books.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const BookService = require("../services/Books");
 const jwtSecurity = require("../security");
-const apiCodes = require("./ERROR_CODES");
+const ERRCodes = require("./ERROR_CODES");
 const router = express.Router();
 
 const path = "/books";
@@ -18,14 +18,14 @@ router.get("/", async (req, res) => {
 
   const params = {};
 
-  if(year) params.year = year;
-  if(author) params.author = author;
+  if (year) params.year = year;
+  if (author) params.author = author;
 
   const books = await booksServices.getBookByParams(params);
 
   res.status(books ? 200 : 404);
   res.json(books || {
-    errorCode: apiCodes.NOT_FOUND.BOOK,
+    errorCode: ERRCodes.NOT_FOUND.BOOK,
     message: "Not found nothing with that parameters"
   });
 });
@@ -37,7 +37,7 @@ router.get("/:id", async (req, res) => {
 
   res.status(book ? 200 : 404);
   res.json(book || {
-    errorCode: apiCodes.NOT_FOUND.BOOK,
+    errorCode: ERRCodes.NOT_FOUND.BOOK,
     message: "Not found any book with that id"
   });
 });
@@ -49,7 +49,22 @@ router.get("/:id/devotionals", async (req, res) => {
 
   res.status(devotionals ? 200 : 404);
   res.json(devotionals || {
-    errorCode: apiCodes.NOT_FOUND.DEVOTIONAL,
+    errorCode: ERRCodes.NOT_FOUND.DEVOTIONAL,
+    message: `Not found any devotional`
+  });
+});
+
+router.get("/:book_id/devotionals/:id", async (req, res) => {
+  const {
+    book_id,
+    id
+  } = req.params;
+
+  const devotionals = await booksServices.getDevotionalById(book_id, id);
+
+  res.status(devotionals ? 200 : 404);
+  res.json(devotionals || {
+    errorCode: ERRCodes.NOT_FOUND.DEVOTIONAL,
     message: `Not found any devotional`
   });
 });
