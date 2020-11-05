3c93bad73f23bd917778c6b41066ef484fa83334
Jonathan <jonicgp97@gmail.com>
2020-11-05 13:00:29 -0600

    Set public URL connection
    Remove environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
-import { DevotionalsController } from './devotionals/devotionals.controller';
-import { DevotionalsService } from './devotionals/devotionals.service';
 import { DevotionalsModule } from './devotionals/devotionals.module';
+
+const MONGODB_URI = 'mongodb+srv://public:Wb3RBrv0hKPCAGWw@cluster0.80fv7.mongodb.net/devotionals?retryWrites=true&w=majority';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        uri: config.get<string>('MONGODB_URI')
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot(MONGODB_URI),
     BooksModule,
     DevotionalsModule,
   ],
