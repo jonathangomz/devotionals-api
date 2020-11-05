9a2470c97625d0cf26643b1f81e256560c783333
Jonathan <jonicgp97@gmail.com>
2020-11-05 13:09:30 -0600

    Add endpoints for today and one devotional

--- src/devotionals/devotionals.controller.ts
+++ src/devotionals/devotionals.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { DevotionalsService } from './devotionals.service';
 
@@ -14,4 +14,22 @@ export class DevotionalsController {
     .catch((err) => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err))
   }
 
+  @Get('today')
+  findToday(@Res() res: Response): void {
+    this.devotionalsService.findToday()
+    .then((devotionals) => res.status(HttpStatus.OK).json(devotionals))
+    .catch((err) => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err))
+  }
+
+  @Get(':id')
+  findOne(@Res() res: Response, @Param('id') id: string): void {
+    this.devotionalsService.findOne(id)
+    .then((devotional) => {
+      if(devotional)
+        res.status(HttpStatus.OK).json(devotional)
+      else
+        res.status(HttpStatus.NOT_FOUND).json({})
+    })
+    .catch((err) => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err))
+  }
 }
