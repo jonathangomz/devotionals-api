2467a055070a9c894e36f28868bff357defab0ce
jonathangomz <jonicgp97@gmail.com>
2021-04-20 11:42:12 -0500

    Fix import using uppercase

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 import { DevotionalsModule } from './devotionals/devotionals.module';
-import { HomeModule } from './home/Home.module';
+import { HomeModule } from './home/home.module';
 
 const MONGODB_URI = 'mongodb+srv://public:Wb3RBrv0hKPCAGWw@cluster0.80fv7.mongodb.net/devotionals?retryWrites=true&w=majority';
 
