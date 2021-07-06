def6a1b623ca1b1730c2aa1c8d28d5a4e3d3ef0c
jonathangomz <jonicgp97@gmail.com>
2021-07-06 17:41:11 -0500

    Add endpoint for today devotional for book

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { Devotional } from 'src/devotionals/devotional.model';
 import { DevotionalsService } from 'src/devotionals/devotionals.service';
 import { BooksService } from './books.service';
 
@@ -32,5 +33,18 @@ export class BooksController {
     this.devotionalsService.findAllFromBook(id)
     .then((devotionals) => res.status(HttpStatus.OK).json(devotionals))
     .catch((err) => res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err))
+  }
+
+  @Get(':id/today')
+  async today(@Res() res: Response, @Param('id') id: string): Promise<void> {
+    const today: string = new Date().toISOString().split('T')[0];
+    const devotionals: Devotional[] = await this.devotionalsService.findAllFromBook(id);
+    const devotional_today: Devotional = devotionals.find(devotional => devotional.date === today);
+
+    if(devotional_today) {
+      res.status(HttpStatus.OK).json(devotional_today);
+    } else {
+      res.status(HttpStatus.NOT_FOUND).json({});
+    }
   }
 }
